a1285687efabe9b6a2082ca2d5585844ebd5ae10
Sumin Byeon <suminb@gmail.com>
2022-02-22 23:32:55 +0900

    Getting started with tire temperature monitoring system

---
+++ asurada/asurada.ino
@@ -0,0 +1,103 @@
+#include <stdarg.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <Adafruit_MLX90614.h>
+#include "storage.h"
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+Adafruit_MLX90614 mlx = Adafruit_MLX90614();
+
+sd_t sd;
+file_t file;
+
+void log(const char *shortMessage, const char *longMessage)
+{
+    Serial.println(longMessage);
+
+    lcd.setCursor(0, 0);
+    lcd.print(shortMessage);
+}
+
+void setup()
+{
+    Serial.begin(9600);
+    while (!Serial)
+    {
+        yield();
+    }
+    Serial.println("Booting up...");
+
+    lcd.init();
+    lcd.backlight();
+
+    Serial.println("Initializing temp. sensor...");
+    if (!mlx.begin())
+    {
+        Serial.println("Failed to start temperature sensor");
+    }
+    initSD();
+}
+
+void initSD()
+{
+    Serial.println("Initializing SD...");
+    if (!sd.begin(SD_CONFIG))
+    {
+        log("SD error (1)", "sd.begin() failed");
+        // sd.initErrorHalt(&Serial);
+    }
+    if (!file.open("data.csv", FILE_WRITE))
+    {
+        Serial.println("Could not open data.csv");
+    }
+}
+
+void loop()
+{
+    char temp[8];
+    char line1[17];
+    char line2[17];
+
+    double ambTemp = mlx.readAmbientTempC();
+    double objTemp = mlx.readObjectTempC();
+
+    if (isnan(ambTemp) || isnan(objTemp))
+    {
+        Serial.println("Temperature is read as NaN");
+        return;
+    }
+
+    dtostrf(ambTemp, 5, 1, temp);
+    sprintf(line1, "AMB: %s", temp);
+    dtostrf(objTemp, 5, 1, temp);
+    sprintf(line2, "OBJ: %s", temp);
+    //   sprintf(line1, "AMB: %.01f", ambTemp);
+    //   sprintf(line2, "OBJ: %.01f", objTemp);
+
+    Serial.print(line1);
+    Serial.print(", ");
+    Serial.println(line2);
+
+    if (file)
+    {
+        writeRecord(&file, ambTemp, objTemp);
+    }
+
+    lcd.setCursor(0, 0);
+    lcd.print(line1);
+    lcd.setCursor(0, 1);
+    lcd.print(line2);
+
+    delay(100);
+}
+
+void writeRecord(file_t *fout, double ambTemp, double objTemp)
+{
+    fout->print(millis());
+    fout->print(",");
+    fout->print(ambTemp);
+    fout->print(",");
+    fout->print(objTemp);
+    fout->println();
+    fout->sync();
+}
---
+++ asurada/storage.h
@@ -0,0 +1,33 @@
+#include <SdFat.h>
+#include <sdios.h>
+
+#define SD_FAT_TYPE 2
+#define SD_CS_PIN 10
+
+// Try max SPI clock for an SD. Reduce SPI_CLOCK if errors occur.
+#define SPI_CLOCK SD_SCK_MHZ(50)
+
+// Try to select the best SD card configuration.
+#if HAS_SDIO_CLASS
+#define SD_CONFIG SdioConfig(FIFO_SDIO)
+#elif ENABLE_DEDICATED_SPI
+#define SD_CONFIG SdSpiConfig(SD_CS_PIN, DEDICATED_SPI, SPI_CLOCK)
+#else // HAS_SDIO_CLASS
+#define SD_CONFIG SdSpiConfig(SD_CS_PIN, SHARED_SPI, SPI_CLOCK)
+#endif // HAS_SDIO_CLASS
+
+#if SD_FAT_TYPE == 0
+typedef SdFat sd_t;
+typedef File file_t;
+#elif SD_FAT_TYPE == 1
+typedef SdFat32 sd_t;
+typedef File32 file_t;
+#elif SD_FAT_TYPE == 2
+typedef SdExFat sd_t;
+typedef ExFile file_t;
+#elif SD_FAT_TYPE == 3
+typedef SdFs sd_t;
+typedef FsFile file_t;
+#else // SD_FAT_TYPE
+#error Invalid SD_FAT_TYPE
+#endif // SD_FAT_TYPE
